cb830201254bff2142ba16a367dd8c4edc6c3d15
Edson Galdino De Araujo <45825@tjmt.jus.br>
2025-08-28 13:23:00 -0400

    ajuste url domínio api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,  // Define que é um ambiente de produção
-  apiUrl: 'https://api.mindsteps.com.br',  // URL da API para produção
+  apiUrl: 'https://api.mindsteps.com.br/api',  // URL da API para produção
 };
