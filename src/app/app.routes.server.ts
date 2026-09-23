3a54e69b3dfb1e051de30625e9962b42c02da29a
Edson Galdino <edsongaldino@outlook.com>
2026-09-22 23:40:41 -0300

    feat: add landing page with server routes and UI assets

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,18 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'paciente/atividades/responder/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'paciente/**',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'dashboard/psicologo/pacientes/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
